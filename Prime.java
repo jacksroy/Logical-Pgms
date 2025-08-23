139476dfddf91d530d30ddd01e36feb2dbc58760
jacksroy <sevay1824@gmail.com>
2025-08-23 22:13:33 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,25 @@
+public class Prime{
+    public static void main(String args[]){
+        int num=-4;
+        int count=0;
+        if(num<1){
+            System.out.println("not a prime");
+        }
+        else{
+            for(int i=1;i<=num;i++){
+                if(num%i == 0){
+                    count ++;
+
+                    
+                }
+            }
+            if(count == 2){
+            System.out.println("Prime");
+        }
+        else{
+            System.out.println("  not prime");
+        }
+        }
+        
+    }
+}

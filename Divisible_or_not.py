9743d02f50f88d7ba057da33104823e1be7d527a
jacksroy <sevay1824@gmail.com>
2025-08-26 19:12:28 +0530

    Create Divisible_or_not.py

---
+++ Divisible_or_not.py
@@ -0,0 +1,12 @@
+num,num2=map(int,input("Enter the values:").split(','))
+
+
+
+if num%num2==0:
+    print("Number is Divisible")
+
+else:
+    print("Number is not Divisible")
+
+# Enter the values:6,3
+# Number is Divisible

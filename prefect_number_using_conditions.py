472a268e070db1c031720404e3b3bc9389978a4b
jacksroy <sevay1824@gmail.com>
2025-08-27 19:14:27 +0530

    Create prefect_number_using_conditions.py

---
+++ prefect_number_using_conditions.py
@@ -0,0 +1,20 @@
+num=int(input("Enter Number :"))
+result=0
+if num%2==0 :
+    print("yes 2")
+if num%3==0 :
+    print("yes 3")
+if num%4==0 :
+    print("yes 4")
+if num%5==0 :
+    print("yes 5")
+result=1+2+3
+if(num == result):
+    print("Prefect Number")
+else:
+    print("Not Prefect number")
+
+# Enter Number :6
+# yes 2
+# yes 3
+# Prefect Number

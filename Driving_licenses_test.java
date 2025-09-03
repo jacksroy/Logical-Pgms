9012a0e2db82e3eb517397460910e01338659405
jacksroy <sevay1824@gmail.com>
2025-09-03 21:58:11 +0530

    Create Driving_licenses_test.java

---
+++ Driving_licenses_test.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        System.out.print("Enter you Number:");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println("Do you has Driving License(yes/No)");
+        String st=sc.next().toLowerCase();
+        if(num>=18 && st.equals("yes")){
+            System.out.println("Eligiable for Driveing");}
+        else{
+            System.out.println("not eligiable for driving");
+        }
+        sc.close();
+    }
+}
+//output
+// Enter you Number:19
+// Do you has Driving License(yes/No)
+
+// NO
+// not eligiable for driving

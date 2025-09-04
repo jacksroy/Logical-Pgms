2d5032be327cc03b3c5b405da4f84b0c7280597b
jacksroy <sevay1824@gmail.com>
2025-09-04 20:02:07 +0530

    Create Print numbers from N to 1.java

---
+++ Print numbers from N to 1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Demo{
+    public static void main(String args[]){
+        System.out.print("Enter Number:");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        for(int i=num;i>=1;i--){
+            System.out.println(i);
+
+            
+        }
+    }
+}
+
+//output
+// Enter Number:6
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1

dd4e99c862fcbc0d8e0b88b5de3328890523a66c
jacksroy <sevay1824@gmail.com>
2025-09-15 18:25:04 +0530

    Create sum of 1 to n.java
    Sum of 1 to N numbers

---
+++ sum of 1 to n.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Demo{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter The Number:");
+        int number=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=number;i++){
+            sum=sum+i;
+            
+            }
+            System.out.println("Sum:"+sum);
+        
+    }
+}
+//output
+// Enter The Number:6
+// Sum:21

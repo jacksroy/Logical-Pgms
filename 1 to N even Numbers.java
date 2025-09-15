e3fa546e00e69c94ea59cfcc023feb75d73d2cc0
jacksroy <sevay1824@gmail.com>
2025-09-15 18:14:02 +0530

    Create 1 to N even Numbers.java
    printing 1 to N numbers even numbers only

---
+++ 1 to N even Numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Demo{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter The Number:");
+        int number=sc.nextInt();
+        for(int i=1;i<=number;i++){
+            if (i%2==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
+//output
+// Enter The Number:10
+// 2 4 6 8 10 

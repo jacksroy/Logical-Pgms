ef2aa0c8a32636452264a6c5734847b8e7ada6fc
jacksroy <sevay1824@gmail.com>
2025-12-30 20:58:52 +0530

    1-n

--- Print 1-N without loops/App_2.java
+++ Print 1-N without loops/App_2.java
@@ -6,6 +6,7 @@ public class App_2 {
 
         //Approach 2 :using Java Streams
         IntStream.range(1,201).forEach(e->System.out.println(e));
+        //Approach 3 :using Java Streams
         IntStream.rangeClosed(1, 18).forEach(System.out::println);
     }
     public static void printNumbers(int num){

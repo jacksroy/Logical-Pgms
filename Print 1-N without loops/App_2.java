f69bcc100cf2bba4002fd5d9cae0c40541f0118c
jacksroy <sevay1824@gmail.com>
2026-01-01 19:29:13 +0530

    No loops Done

--- Print 1-N without loops/App_2.java
+++ Print 1-N without loops/App_2.java
@@ -1,19 +1,26 @@
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class App_2 {
     public static void main(String[] args) {
-        printNumbers(1);
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int last =scanner.nextInt();
+        
+        printNumbers(num,last);
 
         //Approach 2 :using Java Streams
-        IntStream.range(1,201).forEach(e->System.out.println(e));
+        IntStream.range(1,25).forEach(e->System.out.println(e));
         //Approach 3 :using Java Streams
         IntStream.rangeClosed(1, 18).forEach(System.out::println);
+        scanner.close();
     }
-    public static void printNumbers(int num){
-        if(num <=100){
+    public static void printNumbers(int num,int last){
+        
+        if(num <=last){
             System.out.println(num);
             num ++;
-            printNumbers(num); //Recursive call
+            printNumbers(num,last); //Recursive call
         }
     }
 }

c850baeea19185ff035cb6dae828131ce8bee7b5
jacksroy <sevay1824@gmail.com>
2026-01-11 10:38:40 +0530

    Reverse Integer

--- Leetcode/Third.java
+++ Leetcode/Third.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 
 class Third {
     public static int reverse(int x) {
-      if(x <= Integer.MIN_VALUE || x>=Integer.MAX_VALUE){
-        return 0;
-      }
+      
+      
       int result =0;
       while(x!=0){
-        result = result *10 + x%10;
-        x/=10;
+        int pop = x %10;
+         x/=10;
+        
+        if(result > Integer.MAX_VALUE/10 ||result ==Integer.MAX_VALUE/10 && pop >7) return 0;
+        if (result < Integer.MIN_VALUE/10 || result == Integer.MIN_VALUE/10 && pop < -8){
+          return 0;
+        }
+        result = result *10 + pop;
+       
       }
       return result;
     }
